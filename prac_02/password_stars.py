ce52bd4de2b1f81b8449d4f65a5076aecc0b137f
Kenneth <kennethwinston.wiratama@my.jcu.edu.au>
2023-11-18 15:23:01 +0800

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,4 +1,5 @@
 def main():
+    """this function will print users password into asterisks """
     password = get_password()
     while len(password) >= 0:
         if len(password) > 0:
@@ -8,10 +9,12 @@ def main():
 
 
 def hide_password(password):
+    """this function will convert the letter into asterisks"""
     print('*' * len(password))
 
 
 def get_password():
+    """this function will ask user for their password"""
     password = input("Enter password: ")
     return password
 
