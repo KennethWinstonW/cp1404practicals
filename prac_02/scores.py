a4077567eed2c5755db7b614af4610235d5a8c64
Kenneth <kennethwinston.wiratama@my.jcu.edu.au>
2023-11-19 15:43:32 +0800

    Converting celsius to fahrenheit, vice versa.

--- prac_02/scores.py
+++ prac_02/scores.py
@@ -5,17 +5,20 @@ MINIMUM_SCORE = 0
 
 
 def main():
+    """This function will ask user for their score."""
     score = float(input("Enter score: "))
     determine_grade(score)
 
 
 def main2():
+    """This function will generate a random score between 0 and 100."""
     score = random.randint(MINIMUM_SCORE, MAXIMUM_SCORE)
     print(score)
     determine_grade(score)
 
 
 def determine_grade(score):
+    """This function will help determine the grade based on the score."""
     if score < MINIMUM_SCORE and score > MAXIMUM_SCORE:
         return "Invalid score"
     elif score >= 90:
