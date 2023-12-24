713fb69b944e5ff1bf0be309db07090ee782c39b
Kenneth <kennethwinston.wiratama@my.jcu.edu.au>
2023-12-24 17:48:24 +0800

    dynamic labels done

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -4,8 +4,10 @@ from kivy.uix.label import Label
 
 NAMES_LIST = ["Bob", "James", "George", "David"]
 
-class DynamicLabelApp (App):
+
+class DynamicLabelApp(App):
     """Main Program - Kivy app to demo dynamic label creation"""
+
     def build(self):
         """Build the Kivy GUI"""
         self.title = "Dynamic Label"
@@ -13,10 +15,11 @@ class DynamicLabelApp (App):
         self.create_labels()
         return self.root
 
-    def create_labels (self):
+    def create_labels(self):
         """Create labels from data and add them to the GUI"""
         for name in NAMES_LIST:
             temp_label = Label(text=name)
             self.root.ids.main.add_widget(temp_label)
+
 
 DynamicLabelApp().run()
