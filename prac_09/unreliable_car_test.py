71996a4b6404ef8e9f420de7d82073e3b11e4c9b
Kenneth <kennethwinston.wiratama@my.jcu.edu.au>
2024-01-10 22:42:20 +0800

    prac09 unreliable car

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,10 @@
+from prac_09.unreliable_car import UnreliableCar
+
+
+def main():
+    my_unreliable_car = UnreliableCar("Prius 1", 100, 40)
+    my_unreliable_car.drive(40)
+    print(my_unreliable_car)
+
+
+main()
